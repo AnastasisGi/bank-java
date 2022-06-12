6d52ccd65a49340f4bac0e90ee8c416282090c21
Ryan Pierre <info@ryanpierre.com>
2022-06-12 23:25:36 +0100

    add transaction history pojo

--- app/src/main/java/bank/java/Printer.java
+++ app/src/main/java/bank/java/Printer.java
@@ -5,16 +5,26 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Printer {
+    private Comparator<ITransaction> sortByTransactionDateAscending = new Comparator<ITransaction>() {
+        public int compare(ITransaction t1, ITransaction t2) {
+            return t1.getDate().compareTo(t2.getDate());
+        }
+    };
+
     private String printCurrency(float amount) {
         return String.format("%.2f", amount);
     }
+
     private String printColumn(String columnData) {
         return String.format("|| %-14s", columnData);
     }
+
     public String printTransaction(ITransaction transaction, float balance) {
         String dateCol = printColumn(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(transaction.getDate()));
-        String creditCol = printColumn(transaction.getType() == TransactionType.WITHDRAWAL ? "" : printCurrency(transaction.getAmount()));
-        String debitCol = printColumn(transaction.getType() == TransactionType.DEPOSIT ? "" : printCurrency(transaction.getAmount()));
+        String creditCol = printColumn(
+                transaction.getType() == TransactionType.WITHDRAWAL ? "" : printCurrency(transaction.getAmount()));
+        String debitCol = printColumn(
+                transaction.getType() == TransactionType.DEPOSIT ? "" : printCurrency(transaction.getAmount()));
         String balanceCol = printColumn(printCurrency(balance));
         return dateCol.concat(creditCol).concat(debitCol).concat(balanceCol).concat("\n");
     }
@@ -22,17 +32,17 @@ public class Printer {
     public String printHistory(List<ITransaction> transactions) {
         StringBuilder output = new StringBuilder();
 
-        transactions.stream().sorted(new Comparator<ITransaction>() {
-            public int compare(ITransaction o1, ITransaction o2) {
-                return o1.getDate().compareTo(o2.getDate());
-            }
-          }).reduce(0.0f, (balance, t) -> { 
-            float newBalance = t.getType() == TransactionType.DEPOSIT ? balance + t.getAmount() : balance - t.getAmount(); 
+        transactions
+        .stream()
+        .sorted(sortByTransactionDateAscending)
+        .reduce(0.0f, (balance, t) -> {
+            float newBalance = t.getType() == TransactionType.DEPOSIT ? balance + t.getAmount()
+                    : balance - t.getAmount();
 
             output.append(printTransaction(t, newBalance));
 
             return newBalance;
-        } , Float::sum);
+        }, Float::sum);
 
         return output.toString();
     }
