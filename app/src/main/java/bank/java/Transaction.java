dbad7de6607cf421e58abe8f1023cc7151f2df69
Ryan Pierre <info@ryanpierre.com>
2022-06-12 21:55:36 +0100

    Commit WIP printer

---
+++ app/src/main/java/bank/java/Transaction.java
@@ -0,0 +1,39 @@
+package bank.java;
+
+import java.time.LocalDateTime;
+import java.time.Clock;
+
+enum TransactionType {
+    DEPOSIT,
+    WITHDRAWAL
+}
+
+public class Transaction {
+    private float amount;
+    private LocalDateTime date;
+    private TransactionType type;
+
+    public Transaction(float amount) {
+        this.amount = amount;
+        this.type = amount > 0 ? TransactionType.DEPOSIT : TransactionType.WITHDRAWAL;
+        this.date = LocalDateTime.now(Clock.systemDefaultZone());
+    }
+
+    public Transaction(float amount, Clock clock) {
+        this.amount = amount;
+        this.type = amount > 0 ? TransactionType.DEPOSIT : TransactionType.WITHDRAWAL;
+        this.date = LocalDateTime.now(clock);
+    }
+
+    public float getAmount() {
+        return this.amount;
+    }
+
+    public LocalDateTime getDate() {
+        return this.date;
+    }
+
+    public TransactionType getType() {
+        return this.type;
+    }
+}
